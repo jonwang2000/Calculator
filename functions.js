83928466e9a69faf2874aaebac2eada7f3a1005b
Jonathan Wang <j2279wan@edu.uwaterloo.ca>
2018-12-02 22:34:41 -0500

    beautified

--- functions.js
+++ functions.js
@@ -1,119 +1,119 @@
 var oneBtn = document.getElementById('calcOne');
-		var twoBtn = document.getElementById('calcTwo');
-		var threeBtn = document.getElementById('calcThree');
-		var fourBtn = document.getElementById('calcFour');
-		var fiveBtn = document.getElementById('calcFive');
-		var sixBtn = document.getElementById('calcSix');
-		var sevenBtn = document.getElementById('calcSeven');
-		var eightBtn = document.getElementById('calcEight');
-		var nineBtn = document.getElementById('calcNine');
-		var zeroBtn = document.getElementById('calcZero');
+var twoBtn = document.getElementById('calcTwo');
+var threeBtn = document.getElementById('calcThree');
+var fourBtn = document.getElementById('calcFour');
+var fiveBtn = document.getElementById('calcFive');
+var sixBtn = document.getElementById('calcSix');
+var sevenBtn = document.getElementById('calcSeven');
+var eightBtn = document.getElementById('calcEight');
+var nineBtn = document.getElementById('calcNine');
+var zeroBtn = document.getElementById('calcZero');
 
-		var decimalBtn = document.getElementById('calcDecimal');
-		var clearBtn = document.getElementById('calcClear');
-		var backspaceBtn = document.getElementById('calcBackspace');
-		var display = document.getElementById('calcDisplay');
+var decimalBtn = document.getElementById('calcDecimal');
+var clearBtn = document.getElementById('calcClear');
+var backspaceBtn = document.getElementById('calcBackspace');
+var display = document.getElementById('calcDisplay');
 
-		var displayVal = '0';
-		var pendingVal;
-		var evalStringArray = []; 
-
-
-		var calcNumBtns = document.getElementsByClassName('calcNumBtn');
-		var calcOperatorBtns = document.getElementsByClassName('calcOperBtn');
-
-		var updateDisplay = (clickObj) => {
-			var btnText = clickObj.target.innerText;
-
-			if(displayVal === '0')
-				displayVal = '';
-
-			displayVal += btnText;
-			display.innerText = displayVal;
-		}
-
-		var performOperation = (clickObj) => {
-			var operator = clickObj.target.innerText;
-
-			switch (operator) {
-				case '+':
-					pendingVal = displayVal;
-					displayVal = '0';
-					display.innerText = displayVal;
-					evalStringArray.push(pendingVal);
-					evalStringArray.push('+')
-					break;
-
-				case '-':
-					pendingVal = displayVal;
-					displayVal = '0';
-					display.innerText = displayVal;
-					evalStringArray.push(pendingVal);
-					evalStringArray.push('-')
-					break;
-
-				case 'x':
-					pendingVal = displayVal;
-					displayVal = '0';
-					display.innerText = displayVal;
-					evalStringArray.push(pendingVal);
-					evalStringArray.push('*')
-					break;
-
-				case '÷':
-					pendingVal = displayVal;
-					displayVal = '0';
-					display.innerText = displayVal;
-					evalStringArray.push(pendingVal);
-					evalStringArray.push('/')
-					break;
-
-				case '=':
-					evalStringArray.push(displayVal);
-					var evaluation = eval(evalStringArray.join(' '));
-					displayVal = evaluation + '';
-					display.innerText = displayVal;
-					evalStringArray = [];
-					
-					break;
-			
-				default:
-					break;
-			}
-		} 
+var displayVal = '0';
+var pendingVal;
+var evalStringArray = [];
 
 
+var calcNumBtns = document.getElementsByClassName('calcNumBtn');
+var calcOperatorBtns = document.getElementsByClassName('calcOperBtn');
+
+var updateDisplay = (clickObj) => {
+    var btnText = clickObj.target.innerText;
+
+    if (displayVal === '0')
+        displayVal = '';
+
+    displayVal += btnText;
+    display.innerText = displayVal;
+}
+
+var performOperation = (clickObj) => {
+    var operator = clickObj.target.innerText;
+
+    switch (operator) {
+        case '+':
+            pendingVal = displayVal;
+            displayVal = '0';
+            display.innerText = displayVal;
+            evalStringArray.push(pendingVal);
+            evalStringArray.push('+')
+            break;
+
+        case '-':
+            pendingVal = displayVal;
+            displayVal = '0';
+            display.innerText = displayVal;
+            evalStringArray.push(pendingVal);
+            evalStringArray.push('-')
+            break;
+
+        case 'x':
+            pendingVal = displayVal;
+            displayVal = '0';
+            display.innerText = displayVal;
+            evalStringArray.push(pendingVal);
+            evalStringArray.push('*')
+            break;
+
+        case '÷':
+            pendingVal = displayVal;
+            displayVal = '0';
+            display.innerText = displayVal;
+            evalStringArray.push(pendingVal);
+            evalStringArray.push('/')
+            break;
+
+        case '=':
+            evalStringArray.push(displayVal);
+            var evaluation = eval(evalStringArray.join(' '));
+            displayVal = evaluation + '';
+            display.innerText = displayVal;
+            evalStringArray = [];
+
+            break;
+
+        default:
+            break;
+    }
+}
 
 
 
-		for (let i = 0; i < calcNumBtns.length; i++) {
-			calcNumBtns[i].addEventListener('click', updateDisplay, false)
-		}
-
-		for (let i = 0; i < calcOperatorBtns.length; i++) {
-			calcOperatorBtns[i].addEventListener('click', performOperation, false)
-		}
 
 
-		clearBtn.onclick = () => {
-			displayVal = '0';
-			pendingVal = undefined;
-			evalStringArray = [];
-			display.innerText = displayVal;
-		}
+for (let i = 0; i < calcNumBtns.length; i++) {
+    calcNumBtns[i].addEventListener('click', updateDisplay, false)
+}
 
-		backspaceBtn.onclick = () => {
-			let lengthofDisplayVal = displayVal.length;
-			displayVal = displayVal.slice(0, lengthofDisplayVal - 1);
+for (let i = 0; i < calcOperatorBtns.length; i++) {
+    calcOperatorBtns[i].addEventListener('click', performOperation, false)
+}
 
-			if(displayVal === '')
-				displayVal = '0';
 
-			display.innerText = displayVal;
-		}
+clearBtn.onclick = () => {
+    displayVal = '0';
+    pendingVal = undefined;
+    evalStringArray = [];
+    display.innerText = displayVal;
+}
 
-		decimalBtn.onclick = () => {
-			if(!displayVal.includes('.'))
-				displayVal += '.';
-			display.innerText = displayVal;
-		}
+backspaceBtn.onclick = () => {
+    let lengthofDisplayVal = displayVal.length;
+    displayVal = displayVal.slice(0, lengthofDisplayVal - 1);
+
+    if (displayVal === '')
+        displayVal = '0';
+
+    display.innerText = displayVal;
+}
+
+decimalBtn.onclick = () => {
+    if (!displayVal.includes('.'))
+        displayVal += '.';
+    display.innerText = displayVal;
+}
